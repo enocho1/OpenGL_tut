e156cb76bdbaba0e06094187b9469863011fd4d6
enocho1 <13eomale@mtsn.org.uk>
2023-04-13 04:16:34 -0400

    glew stuff

--- src/application.cpp
+++ src/application.cpp
@@ -1,39 +1,75 @@
 #include <iostream>
 
+#include<GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "enoch omale graphics.", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+	//glew initialisation
+	if (glewInit() != GLEW_OK) {
+		std::cout << "error with glew init. " << std::endl;
+	}
 
-    glfwTerminate();
-    return 0;
+	//check version
+	std::cout << "using: " << glGetString(GL_VERSION) << " :)" << std::endl;
+
+	float triangle_verts[6] = {
+	   -0.5, -0.5,
+		0.0,  0.5,
+		0.5, -0.5
+	};
+
+	//vertex buffer definition
+
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), triangle_verts, GL_STATIC_DRAW);
+
+
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//old gl draw triangle
+		/*glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5, -0.5);
+		glVertex2f(0.0, 0.5);
+		glVertex2f(0.5, -0.5);
+		glEnd();*/
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
